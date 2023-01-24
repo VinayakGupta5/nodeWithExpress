b74ed909bb5e078bfb2b11c8a037ad746287ad86
omeshswindia <omeshkumar@swindia.com>
2023-01-24 18:59:16 +0530

    multiple prod api continue

--- controllers/admin/Product.js
+++ controllers/admin/Product.js
@@ -1,26 +1,51 @@
 const Product = require('../../models/Product')
 exports.postProduct = (req, res, next) => {
-  console.log("req.body", req.body)
-  const product = new Product({
-    pkId: req.body.pkId,
-    name: req.body.name
-  })
-  product.save()
+  Product.findOne({ pkId: req.body.pkId })
     .then(result => {
-      res.send(result)
+      if (result) {
+        res.send({ message: "pkId already exist!" })
+      }
+      else {
+        const product = new Product({
+          pkId: req.body.pkId,
+          name: req.body.name
+        })
+        product.save()
+          .then(result => {
+            res.send(result)
+          })
+          .catch(err => {
+            res.send(err)
+          })
+      }
     })
-    .catch(err => {
-      res.send(err)
-    })
+
 }
 
 exports.postMultipleProducts = (req, res, next) => {
-  Product.insertMany(req.body).then((result => {
-    res.send(result)
-  }))
-  .catch(err=>{
-    console.log(err)
+  var existingProd = []
+  var notExistProd = []
+  req.body.forEach(async (item) => {
+    console.log("item", item)
+    var existingDoc = await Product.findOne({ pkId: item.pkId })
+    console.log("first", existingDoc)
+    if (existingDoc) {
+      existingProd.push(item)
+    }
+    else {
+      notExistProd.push(item)
+    }
   })
+  Product.insertMany(notExistProd)
+    .then(result => {
+      res.send({
+        existingProd: existingProd,
+        successfullyAdded: notExistProd
+      })
+    })
+    .catch(err => {
+      console.log(err)
+    })
 }
 
 exports.getAllProducts = (req, res, next) => {
@@ -34,8 +59,8 @@ exports.getAllProducts = (req, res, next) => {
 }
 
 exports.deleteProduct = (req, res, next) => {
-  Product.deleteOne({pkId:req.params.pkId})
-  .then(result => {
-    res.send(result)
-  })
+  Product.deleteOne({ pkId: req.params.pkId })
+    .then(result => {
+      res.send(result)
+    })
 }
--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -5,6 +5,7 @@ const adminController = require('../../controllers/admin/Product')
 const router = express.Router()
 
 router.post('/addProduct',adminController.postProduct)
+router.post('/addManyProduct',adminController.postMultipleProducts)
 router.get('/getProducts',adminController.getAllProducts)
 router.delete('/deleteProduct/:pkId',adminController.deleteProduct)
 
